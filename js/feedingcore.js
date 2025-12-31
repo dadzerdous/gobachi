51402efc766080a144f5bf50fc116d690f780d92
dadzerdous <dadzaverous@gmail.com>
2025-12-31 00:23:46 -0500

    Update feedingcore.js

--- js/feedingcore.js
+++ js/feedingcore.js
@@ -1,1 +1,45 @@
 
+export function createFeedingSession(config) {
+  const {
+    totalDrops,
+    coopBonusPerPlayer,
+    coopBonusCap
+  } = config;
+
+  let hits = 0;
+  let finished = 0;
+
+  function registerDrop(success) {
+    finished++;
+    if (success) hits++;
+  }
+
+  function isComplete() {
+    return finished >= totalDrops;
+  }
+
+  function getStats(players = 1) {
+    const basePercent = totalDrops > 0
+      ? Math.round((hits / totalDrops) * 100)
+      : 0;
+
+    const coopBonus = Math.min(
+      players * coopBonusPerPlayer,
+      coopBonusCap
+    );
+
+    return {
+      percent: basePercent + coopBonus,
+      hits,
+      misses: totalDrops - hits,
+      drops: totalDrops,
+      players
+    };
+  }
+
+  return {
+    registerDrop,
+    isComplete,
+    getStats
+  };
+}
